04c7b67f57306b8c1d8d9b1716102ad9bd3f9f21
Kaituo Zhang <KaituoZhang@users.noreply.github.com>
2026-09-03 14:26:46 -0500

    Use custom production domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kaituozhang.github.io',
+  site: 'https://kzhang12.com',
   output: 'static',
 });
